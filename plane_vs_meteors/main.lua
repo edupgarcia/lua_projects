fb7a1023bd4b32b5f5066ddb40e92f447a597dc8
Eduardo Pereira Garcia <edupgarcia.ti@gmail.com>
2021-06-10 23:23:05 -0300

    Shooting process finished.

--- plane_vs_meteors/main.lua
+++ plane_vs_meteors/main.lua
@@ -1,7 +1,7 @@
 GAME_OVER = false
 METEORS_MAX = 12
 
--- Main Window
+-- Main WidxMeteorow
 MainWindow = {
   width = 320,
   height = 480,
@@ -36,22 +36,55 @@ Plane = {
   height = 62,
   x = MainWindow.width / 2 - 64 / 2,
   y = MainWindow.height - 64,
+  shootArray = {},
 
   move = function (self)
     if love.keyboard.isDown('w') and self.y > 0 then
       self.y = self.y - 1
-    end
-
-    if love.keyboard.isDown('s') and self.y < (MainWindow.height - self.height) then
+    elseif love.keyboard.isDown('s') and self.y < (MainWindow.height - self.height) then
       self.y = self.y + 1
-    end
-
-    if love.keyboard.isDown('a') and self.x > 0 then
+    elseif love.keyboard.isDown('a') and self.x > 0 then
       self.x = self.x - 1
-    end
-
-    if love.keyboard.isDown('d') and self.x < (MainWindow.width - self.width) then
+    elseif love.keyboard.isDown('d') and self.x < (MainWindow.width - self.width) then
       self.x = self.x + 1
+    end
+  end,
+
+  shoot = function (self)
+    ShootSound:play()
+
+    local newShoot = {
+      x = self.x + self.width / 2 - 6,
+      y = self.y,
+      width = 16,
+      height = 16
+    }
+
+    table.insert(self.shootArray, newShoot)
+  end,
+
+  shootMove = function (self)
+    for idxShoot = #self.shootArray, 1, -1 do
+      if self.shootArray[idxShoot].y > 0 then
+        self.shootArray[idxShoot].y = self.shootArray[idxShoot].y - 1
+      else
+        table.remove(self.shootArray, idxShoot)
+      end
+    end
+  end,
+
+  shootColide = function (self)
+    for idxShoot = #self.shootArray, 1, -1 do
+      local itemShoot = self.shootArray[idxShoot]
+      for idxMeteor = #MeteorsArray, 1, -1 do
+        local itemxMeteor = MeteorsArray[idxMeteor]
+        if Colision(itemShoot.x, itemShoot.y, itemShoot.width, itemShoot.height,
+        itemxMeteor.x, itemxMeteor.y, itemxMeteor.width, itemxMeteor.height) then
+          table.remove(self.shootArray, idxShoot)
+          table.remove(MeteorsArray, idxMeteor)
+          break
+        end
+      end
     end
   end,
 
@@ -59,13 +92,10 @@ Plane = {
     PlaneImage = love.graphics.newImage(Images.explosion)
     self.width = 61
     self.height = 63
-
     love.graphics.draw(PlaneImage, Plane.x, Plane.y)
-
     BackgroundSound:stop()
     ExplosionSound:play()
     Game_overSound:play()
-
     GAME_OVER = true
   end
 }
@@ -102,18 +132,27 @@ Meteor = {
     end
   end,
 
-  colision = function ()
+  colisionCheck = function ()
     for k, meteor in pairs(MeteorsArray) do
-      if Plane.x < meteor.x + meteor.width
-      and meteor.x < Plane.x + Plane.width
-      and meteor.y < Plane.y + Plane.height
-      and Plane.y < meteor.y + meteor.height then
+      if Colision(meteor.x, meteor.y, meteor.width, meteor.height, 
+      Plane.x, Plane.y, Plane.width, Plane.height) then
         Plane:destroy()
       end
     end
 
   end
 }
+
+function Colision(x1, y1, w1, h1, x2, y2, w2, h2)
+  return (x2 < x1 + w1) and (x1 < x2 + w2)
+     and (y1 < y2 + h2) and (y2 < y1 + h1)
+end
+
+local function DrawArray(list, image)
+  for k, value in pairs(list) do
+    love.graphics.draw(image, value.x, value.y)
+  end
+end
 
 -- Load some default values for our rectangle.
 function love.load()
@@ -140,7 +179,6 @@ function love.load()
   math.randomseed(os.time())
 end
 
--- Increase the size of the rectangle every frame.
 function love.update(dt)
   if not GAME_OVER then
     if love.keyboard.isDown('w', 's', 'a', 'd') then
@@ -154,22 +192,24 @@ function love.update(dt)
     end
 
     Meteor:move()
-
-    -- ChecaColisoes()
-    Meteor:colision()
+    Plane:shootMove()
+    Meteor:colisionCheck()
+    Plane:shootColide()
   end
 end
 
--- Draw a coloured rectangle.
-function love.draw()
-  -- In versions prior to 11.0, color component values are (0, 102, 102)
-  -- love.graphics.setColor(0, 0.4, 0.4)
-  -- love.graphics.rectangle("fill", x, y, w, h)
+function love.keypressed(key)
+  if key == 'escape' then
+    love.event.quit()
+  elseif key == 'space' and not GAME_OVER then
+    Plane:shoot()
+  end
+end
 
+function love.draw()
   love.graphics.draw(BackgroundImage, 0, 0)
   love.graphics.draw(PlaneImage, Plane.x, Plane.y)
 
-  for k, meteoro in pairs(MeteorsArray) do
-    love.graphics.draw(MeteoroImage, meteoro.x, meteoro.y)
-  end
+  DrawArray(MeteorsArray, MeteoroImage)
+  DrawArray(Plane.shootArray, ShootImage)
 end
